4594ac689a8f003083f99b3fa3cb7612b38372cd
Hallan Silva <hallan_douglas@hotmail.com>
2024-06-17 18:48:16 -0300

    feat: add test coverage and xdebug library to support, adjust on build.sh

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,14 @@ FROM php:8.1.27-fpm
 RUN apt-get update -y && apt-get install -y openssl zip unzip git libonig-dev libxml2-dev
 RUN apt-get clean
 RUN docker-php-ext-install pdo pdo_mysql mbstring xml
+
+# Instala o Xdebug
+RUN pecl install xdebug && docker-php-ext-enable xdebug
+
+# Configura o Xdebug para cobertura de código (ajuste conforme necessário)
+RUN echo "xdebug.mode=coverage" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+RUN echo "xdebug.client_host=host.docker.internal" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+
 RUN curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 WORKDIR /var/www
 COPY . /var/www
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-set -x;
-
 # create enviorment files
 cp ./.env.example ./.env;
 cp ./.env.example ./.env.testing;
 # .env
-sed -i 's/^DB_PASSWORD=.*/DB_PASSWORD=123/' .env
+sed -i 's/^DB_PASSWORD=.*/DB_PASSWORD=1234/' .env
 sed -i 's/^DB_USERNAME=.*/DB_USERNAME=default/' .env
 sed -i 's/^DB_HOST.*/DB_HOST=db/' .env
 # .env.testing
-sed -i 's/^DB_PASSWORD=.*/DB_PASSWORD=123/' .env.testing
+sed -i 's/^DB_PASSWORD=.*/DB_PASSWORD=1234/' .env.testing
 sed -i 's/^DB_USERNAME=.*/DB_USERNAME=default/' .env.testing
 sed -i 's/^DB_HOST=.*/DB_HOST=db-test/' .env.testing
 sed -i 's/^DB_PORT=.*/DB_PORT=33060/' .env.testing
@@ -17,6 +15,15 @@ sed -i 's/^DB_PORT=.*/DB_PORT=33060/' .env.testing
 # configure, build and run docker
 cp ./docker-compose-example.yml ./docker-compose.yml;
 docker compose up -d --build;
+
+# install dependencies and configure application
+docker exec -u root -i transactions-app chmod -R 777 storage storage/logs;
+docker exec -u root -i transactions-app chmod -R 777 ./docker-compose;
+docker exec -u root -i transactions-app composer install;
+docker exec -u root -i transactions-app php artisan key:generate;
+docker exec -u root -i transactions-app php artisan optimize;
+docker exec -u root -i transactions-app chmod -R 777 storage bootstrap/cache;
+docker exec -u root -i transactions-app php artisan migrate;
 
 # nginx config
 echo "server {
@@ -27,26 +34,23 @@ echo "server {
     index index.php index.html index.htm;
 
     location / {
-        try_files $uri $uri/ /index.php?$query_string;
+        try_files \$uri \$uri/ /index.php?\$query_string;
     }
 
     location ~ \.php$ {
         fastcgi_pass app:9000;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
-        fastcgi_param PATH_INFO $fastcgi_path_info;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        fastcgi_param PATH_INFO \$fastcgi_path_info;
         include fastcgi_params;
     }
 
     location ~ /\.ht {
         deny all;
     }
-}" >> ./docker-compose/nginx/conf.d/default.conf;
-docker compose restart nginx;
+}" > ./docker-compose/nginx/conf.d/default.conf;
+docker restart transactions-nginx;
 
-# install dependencies and configure application
-docker exec -i transactions-app chmod -R 777 storage storage/logs;
-docker exec -i transactions-app composer install;
-docker exec -i transactions-app php artisan key:generate;
-docker exec -i transactions-app php artisan optimize;
-docker exec -i transactions-app chmod -R 777 storage bootstrap/cache;
-docker exec -i transactions-app php artisan migrate;
+# Friendly message
+echo -e "\n---------------------------------------------------------------\n
+| Done! The application is available on port 3001 on your PC. |\n
+---------------------------------------------------------------";
