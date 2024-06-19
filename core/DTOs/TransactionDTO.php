998519888e98fff06ab464d97f1d13ee77581e76
Hallanzera <hallan_douglas@hotmail.com>
2024-06-19 10:30:35 -0300

    fix: adjusting id attribute to accept empty value

--- core/DTOs/TransactionDTO.php
+++ core/DTOs/TransactionDTO.php
@@ -5,11 +5,11 @@ namespace Core\DTOs;
 class TransactionDTO
 {
     public function __construct(
-        public readonly string $id,
-        public readonly int    $person_origin_id,
-        public readonly ?int   $person_destination_id,
-        public readonly string $type,
-        public readonly float  $amount
+        public readonly int     $person_origin_id,
+        public readonly ?int    $person_destination_id,
+        public readonly string  $type,
+        public readonly float   $amount,
+        public readonly ?string $id = null
     ) {
     }
 
